fa5dd8b679cd41e714e2cc68a6a4ed0cdcc22901
BRUNO <BRUNO@ACER-BRUNO>
2020-02-14 08:11:45 -0300

    llll

---
+++ JavaCompleto/src/Program.java
@@ -0,0 +1,18 @@
+
+public class Program {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Olá mundo");
+		System.out.println("Olá mundo");
+		System.out.println("Olá mundo");
+		System.out.println("Olá mundo");
+		System.out.println("Olá mundo");
+		System.out.println();
+		System.out.println();
+
+
+	}
+
+}
